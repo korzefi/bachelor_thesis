2401296f11399fe33dd3483e0aa75953095b6fd9
korzefi <korzeniewski.f@gmail.com>
2022-01-31 23:52:23 +0100

    add cleaning and filtering to data script

--- scripts/utils/grouping_raw_data.py
+++ scripts/utils/grouping_raw_data.py
@@ -10,19 +10,25 @@ DATA_PARENT_PATH = ROOT_PATH + '/data/covid-spike-GISAID/spikeprot0104.tar/spike
 DATA_RAW_FILE_NAME = 'spikeprot0104.fasta'
 SPLIT_FILES_DIR_NAME = 'split_data'
 FILES_NAME_CORE = 'spikeprot_batch_data'
+DIVISION_TECHNIQUE = 'year'
+
 
 class DirHandler:
     split_files_dir_name = 'split_data'
     temporary_fasta_dir_name = 'temp_fasta'
     temporary_csv_dir_name = 'temp_csv'
+    period_root_dir_name = 'periods'
     files_name_core = 'spikeprot_batch_data'
+    first_year_dir = '2019'
+    last_year_dir = '2022'
 
     @staticmethod
     def create_dirs():
         split_data_root_dir = DirHandler.__get_split_data_root_path()
         temporary_fasta_dir = DirHandler.get_temp_fasta_dir_path()
         temporary_csv_dir = DirHandler.get_temp_csv_dir_path()
-        dir_paths = [split_data_root_dir, temporary_fasta_dir, temporary_csv_dir]
+        periods_dirs = DirHandler.get_period_dirs()
+        dir_paths = [split_data_root_dir, temporary_fasta_dir, temporary_csv_dir] + periods_dirs
         for path in dir_paths:
             DirHandler.__create_dir(path)
 
@@ -32,16 +38,16 @@ class DirHandler:
 
     @staticmethod
     def get_temp_fasta_dir_path():
-        return f'{DirHandler.__get_split_data_root_path()}/temp_fasta'
+        return f'{DirHandler.__get_split_data_root_path()}/{DirHandler.temporary_fasta_dir_name}'
 
     @staticmethod
     def get_temp_csv_dir_path():
-        return f'{DirHandler.__get_split_data_root_path()}/temp_csv'
+        return f'{DirHandler.__get_split_data_root_path()}/{DirHandler.temporary_csv_dir_name}'
 
     @staticmethod
     def __create_dir(path):
         try:
-            os.mkdir(path)
+            os.makedirs(path)
         except FileExistsError:
             print(f'{path} already exists')
 
@@ -53,23 +59,65 @@ class DirHandler:
     def delete_temp_csv():
         shutil.rmtree(DirHandler.get_temp_csv_dir_path(), ignore_errors=True)
 
+    @staticmethod
+    def get_files_names(dir_path, extension):
+        file_names = os.listdir(dir_path)
+        return list(map(lambda x: x.rstrip('.' + extension), file_names))
+
+    @staticmethod
+    def get_csv_files_paths():
+        csv_dir_path = DirHandler.get_temp_csv_dir_path()
+        files_names = DirHandler.get_files_names(csv_dir_path, extension='.csv')
+        return list(map(lambda x: csv_dir_path + '/' + x + '.csv', files_names))
+
+    @staticmethod
+    def get_period_dirs():
+        root_dir = DirHandler.get_period_root_dir()
+        periods = [f'{root_dir}/{str(year)}' for year in range(int(DirHandler.first_year_dir), int(DirHandler.last_year_dir)+1)]
+        if DIVISION_TECHNIQUE == 'month':
+            periods = DirHandler.__add_months(periods)
+            return periods
+        if DIVISION_TECHNIQUE == 'quarter':
+            periods = DirHandler.__add_quarters(periods)
+            return periods
+        return periods
+
+    @staticmethod
+    def get_period_root_dir():
+        return f'{DirHandler.__get_split_data_root_path()}/{DirHandler.period_root_dir_name}'
+
+    @staticmethod
+    def __add_months(period_years):
+        months = range(1, 12+1)
+        months = map(lambda x: str(x), months)
+        return [f'{year}/{month}' for month in months for year in period_years]
+
+    @staticmethod
+    def __add_quarters(period_years):
+        quarters = range(1, 4 + 1)
+        quarters = map(lambda x: str(x), quarters)
+        return [f'{year}/{quarter}' for quarter in quarters for year in period_years]
+
+
 
 class BatchSplitter:
     lines_num_each_file = 100
     max_num_of_files = 3
+    start_line_idx = 1
 
     @staticmethod
     def split_to_equal_files():
         max_num_iters = BatchSplitter.__get_iterations_num()
-        start_line_idx = 1
         end_line_idx = BatchSplitter.lines_num_each_file
         filepath_with_name_core = DirHandler.get_temp_fasta_dir_path() + '/' + FILES_NAME_CORE
         raw_data_filepath = DATA_PARENT_PATH + '/' + DATA_RAW_FILE_NAME
 
         for i in range(max_num_iters):
-            copy_command = f"cat {raw_data_filepath} | sed -n '{start_line_idx},{end_line_idx}p' >> {filepath_with_name_core}-{start_line_idx}-{end_line_idx}.fasta"
-            os.system(copy_command)
-            start_line_idx += BatchSplitter.lines_num_each_file
+            copy_command = ['cat', raw_data_filepath, '|', 'sed', '-n',
+                            f'{BatchSplitter.start_line_idx},{end_line_idx}p', '>>',
+                            f"{filepath_with_name_core}-{BatchSplitter.start_line_idx}-{end_line_idx}.fasta"]
+            os.system(' '.join(copy_command))
+            BatchSplitter.start_line_idx += BatchSplitter.lines_num_each_file
             end_line_idx += BatchSplitter.lines_num_each_file
 
     @staticmethod
@@ -90,8 +138,8 @@ class BatchSplitter:
 class CsvTransformer:
     @staticmethod
     def transform_files():
-        files_names = CsvTransformer.__get_files_names()
         fasta_files_dir = DirHandler.get_temp_fasta_dir_path()
+        files_names = DirHandler.get_files_names(dir_path=fasta_files_dir, extension='fasta')
         csv_files_dir = DirHandler.get_temp_csv_dir_path()
         for name in files_names:
             read_file = pd.read_fwf(f'{fasta_files_dir}/{name}.fasta', header=None)
@@ -99,12 +147,6 @@ class CsvTransformer:
             sequence_df = CsvTransformer.__create_sequence_df(csv_file_df=read_file)
             result = pd.concat([description_df, sequence_df], axis=1, join='inner')
             result.to_csv(f'{csv_files_dir}/{name}.csv', index=False)
-
-    @staticmethod
-    def __get_files_names():
-        split_files_dir_filepath = DirHandler.get_temp_fasta_dir_path()
-        file_names = os.listdir(split_files_dir_filepath)
-        return list(map(lambda x: x.rstrip('.fasta'), file_names))
 
     @staticmethod
     def __create_description_df(csv_file_df):
@@ -122,13 +164,97 @@ class CsvTransformer:
 
 
 class BatchCleaner:
+    min_len = 1260
+
     @staticmethod
     def clean():
+        files_paths = DirHandler.get_csv_files_paths()
+        for file in files_paths:
+            df = pd.read_csv(file)
+            df = BatchCleaner.__remove_ambiguous(df)
+            df = BatchCleaner.__remove_short(df)
+            df = BatchCleaner.__filter_description(df)
+            df = BatchCleaner.__remove_duplicates(df)
+            df.to_csv(file, index=False)
+
+    @staticmethod
+    def __remove_ambiguous(df):
+        ambiguous_aminos = ['B', 'J', 'Z', 'X', '-']
+        df = df[~df['sequence'].str.contains('|'.join(ambiguous_aminos))]
+        df.reset_index(drop=True, inplace=True)
+        return df
+
+    @staticmethod
+    def __remove_short(df):
+        end_sign = '*'
+        df = df[df['sequence'].str.endswith(end_sign)]
+        df = df[df['sequence'].str.len() > BatchCleaner.min_len]
+        return df
+
+    @staticmethod
+    def __filter_description(df):
+        splitted_desc = df['description'].str.split(pat='|', expand=True, n=3)
+        splitted_desc.columns = ['gene', 'isolate_name', 'timestamp', 'rest']
+        splitted_desc.drop(columns=['gene', 'rest'], inplace=True)
+        splitted_desc = BatchCleaner.__adapt_day_format(splitted_desc)
+        df.drop(columns='description', inplace=True)
+        df = pd.concat([splitted_desc, df], axis=1, join='inner')
+        return df
+
+    @staticmethod
+    def __adapt_day_format(df):
+        days = pd.to_numeric(df['timestamp'].str[-2:]) + 1
+        df['timestamp'] = df['timestamp'].str[:-2] + days.astype(str)
+        return df
+
+    @staticmethod
+    def __remove_duplicates(df):
+        df.drop_duplicates(subset=['isolate_name'], inplace=True)
+        return df
+
+
+class PeriodSorter:
+    @staticmethod
+    def divide():
+        files_paths = DirHandler.get_csv_files_paths()
+        for file in files_paths:
+            df = pd.read_csv(file)
+            PeriodSorter.__sort(df)
+            df.to_csv(file, index=False)
+            PeriodSorter.__divide(df)
+
+    @staticmethod
+    def __sort(df):
+        df['timestamp'] = pd.to_datetime(df['timestamp'], format='%Y-%m-%d', errors='coerce')
+        df.sort_values(by='timestamp', inplace=True)
+        df.dropna(subset='timestamp', inplace=True)
+        df.reset_index(drop=True, inplace=True)
+        return df
+
+    @staticmethod
+    def __divide(df):
+        types = {'year': PeriodSorter.__divide_by_year,
+                 'quarter': PeriodSorter.__divide_by_quarter,
+                 'month': PeriodSorter.__divide_by_month}
+        types[DIVISION_TECHNIQUE](df)
+
+    @staticmethod
+    def __divide_by_year(df):
+        pass
+
+    @staticmethod
+    def __divide_by_quarter(df):
+        pass
+
+    @staticmethod
+    def __divide_by_month(df):
         pass
 
 
 if __name__ == '__main__':
     DirHandler.create_dirs()
-    BatchSplitter.split_to_equal_files()
-    CsvTransformer.transform_files()
+    # BatchSplitter.split_to_equal_files()
+    # CsvTransformer.transform_files()
     # DirHandler.delete_temp_fasta()
+    # BatchCleaner.clean()
+    # PeriodSorter.divide()
